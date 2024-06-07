f7605b24c709ca82a1d151b955bf3f11e3e5106a
Alfie Anil <alfieanil@gmail.com>
2024-06-07 15:07:10 +0100

    Trips added to firestore

--- components/MileageTracker.jsx
+++ components/MileageTracker.jsx
@@ -1,10 +1,12 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Alert } from "react-native";
 import { useState, useEffect } from "react";
-import { Button } from "@rneui/base";
 import { StatusBar } from 'expo-status-bar';
 import * as Location from 'expo-location';
 import harvesine from 'haversine-distance';
-import { PaperProvider } from "react-native-paper";
+import { Dialog, Modal, PaperProvider, Portal, TextInput, Button, Snackbar } from "react-native-paper";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { addDoc, collection, doc } from "firebase/firestore";
+import { auth, db } from "../firebaseConfig";
 
 export default function HomeScreen() {
     const [travel, setTravel] = useState({
@@ -14,6 +16,12 @@ export default function HomeScreen() {
     
       const [monitor, setMonitor] = useState(false);
       const [watchId, setWatchId] = useState(null);
+
+      const [modalVisible, setModalVisible] = useState(false);
+      const [travelName, setTravelName] = useState('');
+
+      const [snackbarText, setSnackbarText] = useState('');
+      const [snackbarVisible, setSnackBarVisible] = useState(false);
     
       useEffect(() => {
     
@@ -81,8 +89,79 @@ export default function HomeScreen() {
         }
       }, [monitor])
 
+      const findDestination = async() => {
+        let geocode = await Location.reverseGeocodeAsync({
+            longitude: travel.positions[travel.positions.length-1].longitude,
+            latitude: travel.positions[travel.positions.length-1].latitude,
+        });
+
+        console.log(geocode[0].name + ', ' + geocode[0].city, ", ", geocode[0].region);
+        return geocode[0];
+      }
+
+      const checkEndTrip = () => {
+        Alert.alert('Are you sure?', 'Are you sure you want to end this trip?', [
+            {
+                'text': 'No, keep going',
+                onPress: () => {console.log("Keep going"); setMonitor(true)}
+            },
+            {
+                text: 'Yes, end trip',
+                onPress: () => setModalVisible(true)
+            }
+        ])
+      }
+
+      const recordTripWithDestination = async() => {
+        let tripName = await findDestination();
+        let tripNameFull = tripName.name + ', ' + tripName.city + ', ' + tripName.region;
+        recordTrip(tripNameFull);
+      }
+      
+      const recordTrip = async(tripName) => {
+        setModalVisible(false);
+        console.log("Journey Name: ", tripName);
+        try {
+            const docRef = await addDoc(collection(doc(db, 'users', auth.currentUser.uid), 'trips'), {
+                tripName: tripName,
+                timestamp: new Date(),
+                totalDistance: travel.totalDistance
+            });
+
+            resetTrip();
+            showSnackBar("Trip recorded successfully!")
+        } catch(e) {
+            console.log("Exception: ", e)
+            showSnackBar("There was an error when recording this trip.")
+        }
+      }
+
+      const showSnackBar = (text) => {
+        setSnackbarText(text);
+        setSnackBarVisible(true);
+      }
+
+      const resetTrip = () => {
+        setTravelName('');
+        setTravel({
+            'positions': [],
+            'totalDistance': 0
+          });
+      }
+
       return (
         <PaperProvider>
+            <Portal>
+                <Modal visible={modalVisible} onDismiss={() => {setModalVisible(travelName === '')}} contentContainerStyle={{backgroundColor: "white", padding: 20, marginHorizontal: 15, alignItems: 'center'}} >
+                    <Text style={[styles.promptHeading, {marginBottom: 8}]}>Give your journey a Name</Text>
+
+                    <TextInput mode="outlined" style={styles.travelInput} value={travelName} onChangeText={(value) => setTravelName(value)} />
+                    <Button onPress={() => recordTrip(travelName)} mode="contained" style={{width: "100%", marginTop: 4}}>Submit</Button>
+
+                    <Button icon="map-marker"  onPress={recordTripWithDestination} style={{marginTop: 16}}>Use my current location instead</Button>
+                </Modal>
+            </Portal>
+
             <View style={styles.container}>
             {/* <Text style={styles.milesHeading}>Distance Travelled</Text> */}
             <View style={[{width: '100%', flexDirection: 'row', justifyContent: 'center', alignContent: 'flex-end', alignItems: 'center'}]}>
@@ -93,11 +172,15 @@ export default function HomeScreen() {
             <View>
             <Button disabled={monitor} type="clear" titleStyle={styles.startButton} onPress={() => setMonitor(true)}>Start</Button>
 
-            <Button disabled={!monitor} type='clear'titleStyle={styles.endButton} onPress={() => setMonitor(false)}>End</Button>
+            <Button disabled={!monitor} type='clear'titleStyle={styles.endButton} onPress={() => {setMonitor(false); checkEndTrip()}}>End</Button>
+
+            {/* <Button type="clear" onPress={findDestination}>Find Destination</Button> */}
             </View>
             
             {/* <Text>{travel.totalDistance} metres</Text> */}
             <StatusBar style="auto" />
+
+            <Snackbar visible={snackbarVisible} onDismiss={() => setSnackBarVisible(false)} action={{label: 'Okay', onPress: () => setSnackBarVisible(false)}}>{snackbarText}</Snackbar>
             </View>
         </PaperProvider>
       );
@@ -125,5 +208,14 @@ const styles = StyleSheet.create({
     endButton: {
       fontSize: 20,
       color: "red"
+    },
+
+    promptHeading: {
+        fontSize: 20,
+        fontWeight: 'bold'
+    },
+
+    travelInput: {
+        width: "100%"
     }
   });
